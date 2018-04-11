dd9a992dd9dd3544d2fb5c5334126cd02828c56f
David Reid <davidfreid97@gmail.com>
2018-04-11 15:52:09 +0000

    Create Upload form with description and photo fields

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,12 @@
 
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import StringField
+from wtforms.validators import DataRequired, Email
+
+class UploadForm (FlaskForm):
+    description = StringField('Description', validators=[DataRequired()])
+    photo = FileField('Photo', validators=[
+        FileRequired(),
+        FileAllowed(['jpg', 'png', 'Images only!'])
+    ])
